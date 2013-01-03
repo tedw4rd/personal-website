37ecc5774e7d9acca50b240dc622eb9f4ee08468
Ted Aronson <ted@mindsnacks.com>
2013-01-02 20:58:57 -0800

    database driven site, with square layout

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URI")
+app.config['CSRF_ENABLED'] = False
+app.secret_key = os.environ.get("SECRET_KEY")
+
+db = SQLAlchemy(app)
+
+from models import *
+
+db.create_all()
+
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqlamodel import ModelView
+
+admin = Admin(app)
+
+admin.add_view(ModelView(LinkCategory, db.session))
+admin.add_view(ModelView(Link, db.session))
 
 @app.route("/")
 def show_home():
-    return render_template("index.html")
+    cat_query = LinkCategory.query.all()
+    all_cats = []
+    for cat in cat_query:
+        all_cats.append({"text": cat.name, "url":"/" + cat.stub, "description":""})
+    return render_template("blocks.html", links=all_cats, trail=[])
+
+
+@app.route("/<stub>/all")
+def show_category(stub):
+    cat = LinkCategory.query.filter_by(stub=stub).first()
+    if cat is None:
+        abort(404)
+    display_links = []
+    for l in cat.links:
+        display_links.append({"text":l.display_text, "url":l.url, "description":l.description})
+
+    return render_template("blocks.html", links=display_links, trail=[{"link": stub, "text":cat.name, "current":False},{"link": stub, "text":"ALL", "current":True}])
+
+
+@app.route("/<stub>")
+def show_category(stub):
+    cat = LinkCategory.query.filter_by(stub=stub).first()
+    if cat is None:
+        abort(404)
+    links = cat.links[:5]
+    display_links = []
+    for l in links:
+        display_links.append({"text":l.display_text, "url":l.url, "description":l.description})
+
+    if len(links) > 5:
+        display_links.append({"text":"ALL", "url":"/%s/%s" % (stub, "all"), "description": ""})
+
+    return render_template("blocks.html", links=display_links, trail=[{"link": stub, "text":cat.name, "current":True}])
 
 
 @app.route("/stylesheets/<path:to_file>")
---
+++ website/models.py
@@ -0,0 +1,18 @@
+from website import db
+
+class LinkCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True)
+    stub = db.Column(db.String(128), unique=True)
+    links = db.relationship('Link', backref="category", cascade="all, delete, delete-orphan")
+
+    def __str__(self):
+        return self.name
+
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    display_text = db.Column(db.String(128))
+    url = db.Column(db.String(512))
+    description = db.Column(db.Text)
+    category_id = db.Column(db.Integer, db.ForeignKey('link_category.id'))
+
